c5ec1c24f1b18ee9df777fe9072fcfa4d2074d11
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-07-25 21:40:52 +0700

    First version of bands_Si_fcc

---
+++ UntukWebCMD/bands_Si_fcc_01/gen_kpts.py
@@ -0,0 +1,32 @@
+from ase.dft.kpoints import *
+from ase.units import Bohr
+import sys
+
+# Any lattice parameter should work, the important ones are the
+# lattice vectors v1, v2, and v3. In this case we used the definition
+# of FCC lattice vectors used in PWSCF.
+alat = 10.20*Bohr
+v1 = [-1,0,1]
+v2 = [ 0,1,1]
+v3 = [-1,1,0]
+cell = 0.5*alat*np.transpose( np.array( [v1, v2, v3] ) )
+
+
+# Number of total k-points in the path
+NKPT = 60
+# Use ase.dft module for obtaining k-points along high symmetry directions
+points = ibz_points['fcc']
+G = points['Gamma']
+G1 = [1.0, 1.0, 1.0]
+X = points['X']
+L = points['L']
+kpts, x, Xkpt = get_bandpath([L, G, X, G1], cell, npoints=NKPT)
+
+# Write kpts in the format understood by PWSCF
+# The weights of the k-points are not used, so they can take any value.
+# In this case we set them all to 1.0
+sys.stdout.write("K_POINTS crystal\n")
+sys.stdout.write("%d\n" % NKPT)
+for ik in range(NKPT):
+  sys.stdout.write('%.8f %.8f %.8f %.8f\n' % (kpts[ik,0],kpts[ik,1],kpts[ik,2],x[ik]))
+
---
+++ UntukWebCMD/bands_Si_fcc_01/plot_bands_gnu_v2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+NBANDS   = 8
+NKPOINTS = 60
+
+databands = np.loadtxt("bands.out.gnu")
+
+ebands = np.zeros( (NKPOINTS, NBANDS) )
+kvec   = np.zeros( (NKPOINTS, NBANDS) )
+
+for ib in range(NBANDS):
+    idx1 = (ib)*NKPOINTS
+    idx2 = (ib+1)*NKPOINTS
+    ebands[:,ib] = databands[idx1:idx2,1]
+    kvec[:,ib]   = databands[idx1:idx2,0]
+
+Nocc = 4  # number of occupied bands
+Efermi = np.max( ebands[:,Nocc-1] )
+ebands[:,:] = ebands[:,:] - Efermi
+
+Emin = np.min(ebands)
+Emax = np.max(ebands)
+
+plt.figure(figsize=(6, 8))
+plt.clf()
+for ib in range(NBANDS):
+    #plt.plot( kvec[:,ib], ebands[:,ib], marker='o' )
+    plt.plot( kvec[:,ib], ebands[:,ib] )
+
+# obtained from the output log of bands.x
+Xkpt = [0.0000, 0.8660, 1.8660, 3.2802]
+labelX = ["L", r"$\Gamma$", "X", r"$\Gamma$"]
+
+for p in Xkpt:
+    plt.plot([p, p], [Emin, Emax], 'k-')
+plt.xticks(Xkpt, labelX)
+
+plt.plot([0, Xkpt[-1]], [0, 0], 'k--')
+plt.ylim( Emin, Emax )
+plt.xlim( 0, Xkpt[-1] )
+
+plt.xlabel('k vector')
+plt.ylabel('Energy (eV)')
+plt.title('Band structure of Si')
+plt.savefig('Si_bands_v3.pdf')
+
+
