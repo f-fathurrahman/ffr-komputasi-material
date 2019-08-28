a380ceabfa9cc7f1cd7976830b788fca8cf362ca
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-08-28 22:17:06 +0700

    Remove type annotations for init_FD1d_grid

--- ComputationalQM/julia/1d/init_FD1d_grid.jl
+++ ComputationalQM/julia/1d/init_FD1d_grid.jl
@@ -8,4 +8,4 @@ function init_FD1d_grid( x_min::Float64, x_max::Float64, N::Int64 )
     return x, h
 end
 
-init_FD1d_grid( X::Tuple{Float64,Float64}, N::Int64) = init_FD1d_grid( X[1], X[2], N )
+init_FD1d_grid( X, N ) =init_FD1d_grid( X[1], X[2], N )
