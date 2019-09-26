af79e8023281e8c45712abffe12658866d1bdfdd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-09-26 12:08:25 +0700

    Using pgfplotsx instead of PyPlot

--- ComputationalQM/julia/3d/build_nabla2_matrix.jl
+++ ComputationalQM/julia/3d/build_nabla2_matrix.jl
@@ -6,25 +6,14 @@ include("../1d/build_D2_matrix_9pt.jl")
 const ⊗ = kron
 
 function build_nabla2_matrix( fdgrid::FD3dGrid; func_1d=build_D2_matrix_3pt )
-    
-    Nx = fdgrid.Nx
-    hx = fdgrid.hx
 
-    Ny = fdgrid.Ny
-    hy = fdgrid.hy
+    D2x = func_1d(fdgrid.Nx, fdgrid.hx)
+    D2y = func_1d(fdgrid.Ny, fdgrid.hy)
+    D2z = func_1d(fdgrid.Nz, fdgrid.hz)
 
-    Nz = fdgrid.Nz
-    hz = fdgrid.hz
-
-    D2x = func_1d(Nx, hx)
-    D2y = func_1d(Ny, hy)
-    D2z = func_1d(Nz, hz)
-
-    IIx = speye(Nx)
-    IIy = speye(Ny)
-    IIz = speye(Nz)
-
-    #∇2 = kron(D2x, speye(Ny)) + kron(speye(Nx), D2y)
+    IIx = speye(fdgrid.Nx)
+    IIy = speye(fdgrid.Ny)
+    IIz = speye(fdgrid.Nz)
 
     ∇2 = D2x⊗IIy⊗IIz + IIx⊗D2y⊗IIz + IIx⊗IIy⊗D2z 
 
