adfd9a210703b10497fe7dd3f7b4adac6f219553
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-01-14 15:00:01 +0700

    More reorganization

--- TALKS/ImplementingDFT_01/codes/sch_1d/main_poeschl_teller.jl
+++ TALKS/ImplementingDFT_01/codes/sch_1d/main_poeschl_teller.jl
@@ -6,11 +6,11 @@ import PyPlot
 const plt = PyPlot
 plt.rc("text", usetex=true)
 
-include("init_FD1d_grid.jl")
-include("build_D2_matrix_3pt.jl")
+include("INC_sch_1d.jl")
 
-function pot_harmonic( x; ω=1.0 )
-    return 0.5 * ω^2 * x^2
+# one-dimensional Poeschl-Teller potential
+function pot_poeschl_teller( x; λ::Int64=1 )
+    return -0.5*λ*(λ + 1)*sech(x)^2
 end
 
 function main()
@@ -20,18 +20,21 @@ function main()
     N = 51
     x, h = init_FD1d_grid(xmin, xmax, N)
     # Build 2nd derivative matrix
-    D2 = build_D2_matrix_3pt(N, h)
+    D2 = build_D2_matrix_9pt(N, h)
     # Potential
-    Vpot = pot_harmonic.(x)
+    λ = 5
+    Vpot = pot_poeschl_teller.(x, λ=λ)
     # Hamiltonian
     Ham = -0.5*D2 + diagm( 0 => Vpot )
     # Solve the eigenproblem
     evals, evecs = eigen( Ham )
-    # We will show the 5 lowest eigenvalues
-    Nstates = 5
+    # Show the bound states energies
+    Nstates = λ
     @printf("Eigenvalues\n")
     for i in 1:Nstates
-        @printf("%5d %18.10f\n", i, evals[i])
+        μ = (λ - i + 1)
+        E_analytic = -0.5*μ^2
+        @printf("%5d %18.10f %18.10f %18.10e\n", i, evals[i], E_analytic, abs(E_analytic-evals[i]))
     end
 
     # normalize the first three eigenstates
@@ -41,13 +44,14 @@ function main()
     end
 
     # Plot up to 3rd eigenstate
+    # Plot up to 3rd eigenstate
     plot_title = "N="*string(N)
     plt.plot(x, evecs[:,1], label="1st eigenstate", marker="o")
     plt.plot(x, evecs[:,2], label="2nd eigenstate", marker="o")
     plt.plot(x, evecs[:,3], label="3rd eigenstate", marker="o")
     plt.legend()
     plt.tight_layout()
-    plt.savefig("IMG_main_harmonic_01_"*string(N)*".pdf")
+    plt.savefig("IMG_main_poeschl_teller_"*string(N)*".pdf")
 end
 
 main()
