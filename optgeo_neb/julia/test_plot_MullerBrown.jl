06b6bac7467e703a459896f22f4c893d4ac7f97d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-08-29 00:36:38 +0700

    Cutoff positive potential. Plot two minima.

--- optgeo_neb/julia/test_plot_MullerBrown.jl
+++ optgeo_neb/julia/test_plot_MullerBrown.jl
@@ -12,7 +12,7 @@ function do_plot()
     mb = MullerBrown()
     Nx = 200
     Ny = 200
-    xgrid = range(-1.7, stop=0.5, length=Nx)
+    xgrid = range(-1.7, stop=1.0, length=Nx)
     ygrid = range(-0.5, stop=2.0, length=Ny)
     Vpot = zeros(Float64,Nx,Ny)
     forces = zeros(Float64,3,1) # not used
@@ -20,16 +20,27 @@ function do_plot()
         x = xgrid[i]
         y = ygrid[j]
         Vpot[i,j] = calc_energy_forces!(mb, x, y, forces)
+        # Cutoff the potential (to obtain easier visualization)
+        if Vpot[i,j] > 1.0
+            Vpot[i,j] = 1.0
+        end
     end
+
+    println("maximum(Vpot) = ", maximum(Vpot))
+
+    x1 = -0.5582247
+    y1 =  1.44172487
+
+    x2 = 0.62362776
+    y2 = 0.02813212
 
     plt.clf()
     #plt.surf(xgrid, ygrid, transpose(Vpot))
     #plt.contourf(xgrid, ygrid, transpose(Vpot), levels=20)
-    plt.contour(xgrid, ygrid, transpose(Vpot), levels=20)
-    plt.axis("square")
+    plt.contour(xgrid, ygrid, transpose(Vpot), levels=10)
     plt.axis("equal")
-    #plt.xticks([]); plt.yticks([])
-    #plt.text(-4.5, 4.5, "State "*string(ist))
+    plt.plot([x1], [y1], marker="*", color="black")
+    plt.plot([x2], [y2], marker="o", color="red")
     plt.tight_layout()
     plt.savefig("IMG_MullerBrown.pdf")
 end
