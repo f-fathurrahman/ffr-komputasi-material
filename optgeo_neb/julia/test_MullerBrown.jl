3e481d48c3ee2d29f660e8cf9f00afb57dbfa124
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-08-28 23:40:01 +0700

    Remove repetitive codes

--- optgeo_neb/julia/test_MullerBrown.jl
+++ optgeo_neb/julia/test_MullerBrown.jl
@@ -1,8 +1,12 @@
+using Printf
+
+include("Atoms.jl")
 include("MullerBrown.jl")
 
 function main()
     mb = MullerBrown()
-    energy, forces = calc_energy_forces(mb, -0.55, 1.30)
+    forces = zeros(Float64,3,1)
+    energy = calc_energy_forces!(mb, -0.55, 1.30, forces)
     println(energy)
     println(forces)
 end
