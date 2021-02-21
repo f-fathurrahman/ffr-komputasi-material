ed5d523d72fec0d7079cb30cfde309e77c1b7e42
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-22 06:24:15 +0700

    Test Fonon diatomik

--- FisikaZatPadat/Fonon_diatomik.py
+++ FisikaZatPadat/Fonon_diatomik.py
@@ -7,12 +7,22 @@ m1, m2 = symbols("m1 m2", real=True, positive=True)
 κ1, κ2 = symbols("kappa1 kappa2", real=True, positive=True)
 ω = symbols("omega", real=True)
 
-M = Matrix([
-    [2*((κ1 + κ2) - κ2*cos(k*a)) - m1*ω**2, -κ1*(1 + cos(k*a) - I*sin(k*a))],
-    [-κ1*(1 + cos(k*a) + I*sin(k*a)), 2*((κ1 + κ2) - κ2*cos(k*a)) - m2*ω**2]
+from sympy import *
+init_printing(use_unicode=True)
+
+k = symbols("k", real=True)
+a = symbols("a", real=True, positive=True)
+m1, m2 = symbols("m1 m2", real=True, positive=True)
+κ1, κ2 = symbols("kappa1 kappa2", real=True, positive=True)
+ω = symbols("omega", real=True)
+
+
+secMat = Matrix([
+    [(κ1 + κ2) - m1*ω**2, -κ2 - κ1*(cos(k*a) + I*sin(k*a)) ],
+    [-κ2 - κ1*(cos(k*a) - I*sin(k*a)), (κ1 + κ2) - m2*ω**2]
 ])
 
-eq1 = M.det()
+eq1 = secMat.det()
 dict_num = {
     m1: 1.0,
     m2: 1.0,
@@ -46,4 +56,4 @@ for k_n in k_nums:
     print("done k_n = ", k_n)
 
 plt.grid(True)
-plt.savefig("IMG_Simon_10_4_v1.pdf")
+plt.savefig("IMG_fonon_2d_v1.pdf")
