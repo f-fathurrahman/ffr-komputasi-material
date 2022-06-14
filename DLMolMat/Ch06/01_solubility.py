c302b385afc14663d923cd1b50718a75e77e7db5
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-06-14 21:04:55 +0700

    Finish until plot

--- DLMolMat/Ch06/01_solubility.py
+++ DLMolMat/Ch06/01_solubility.py
@@ -20,5 +20,52 @@ test_N = int(0.1 * N)
 test_data = full_data.take(test_N).batch(16)
 train_data = full_data.skip(test_N).batch(16)
 
-print(test_data)
-print(train_data)
+
+# Build a neural network
+
+# Hidden layer, only need to define output dimension
+hidden_layer = tf.keras.layers.Dense(32, activation="tanh")
+
+# Last layer. We want to output one number
+output_layer = tf.keras.layers.Dense(1)
+
+# Now put the layers into a sequential model
+model = tf.keras.Sequential()
+model.add(hidden_layer)
+model.add(output_layer)
+
+# Try our model on several datapoints
+res = model(soldata[feature_names].values[:3])
+print(res)
+
+# Prepare the model for training
+model.compile(optimizer="SGD", loss="mean_squared_error")
+
+# Train the model
+model.fit(train_data, epochs=50)
+
+# get model predictions on test data and get labels
+# squeeze to remove extra dimensions
+yhat = np.squeeze(model.predict(test_data))
+test_y = soldata["Solubility"].values[:test_N]
+
+# get model predictions on test data and get labels
+# squeeze to remove extra dimensions
+yhat = np.squeeze(model.predict(test_data))
+test_y = soldata["Solubility"].values[:test_N]
+
+plt.plot(test_y, yhat, ".")
+plt.plot(test_y, test_y, "-")
+plt.xlabel("Measured Solubility $y$")
+plt.ylabel("Predicted Solubility $\hat{y}$")
+plt.text(
+    min(test_y) + 1,
+    max(test_y) - 2,
+    f"correlation = {np.corrcoef(test_y, yhat)[0,1]:.3f}",
+)
+plt.text(
+    min(test_y) + 1,
+    max(test_y) - 3,
+    f"loss = {np.sqrt(np.mean((test_y - yhat)**2)):.3f}",
+)
+plt.savefig("IMG_01_solubility_01.pdf")
