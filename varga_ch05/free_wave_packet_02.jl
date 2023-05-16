160d8780dcd014b4bef4ee63a70396483798023b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-05-16 12:31:47 +0700

    Using Laplacian, try to vary k0

--- varga_ch05/free_wave_packet_02.jl
+++ varga_ch05/free_wave_packet_02.jl
@@ -9,7 +9,7 @@ include("build_D2_matrix_3pt.jl")
 include("build_D2_matrix_11pt.jl")
 
 function build_Ham(N::Int64, Δx::Float64)
-    D2 = build_D2_matrix_3pt(N, Δx)
+    D2 = build_D2_matrix_11pt(N, Δx)
     return -0.5*D2
 end
 
@@ -38,14 +38,25 @@ function gaussian_wavepacket(x, t, a0, p0)
 end
 
 
-#function main()
-
-    Nx = 500
-    Nt = 500
-    Δt = 0.001
+function main()
 
     a = -25.0
     b =  25.0
+
+    σ = sqrt(1.0)
+    k0 = 20.0
+    x0 = 0.0
+
+    k_grid_min = 2*k0 * 10
+    Δx = 2π/k_grid_min
+    println("Δx min = ", Δx)
+    Nx = floor(Int64, (b - a)/Δx) + 2
+    println("Calculated Nx = ", Nx)
+
+    if Nx < 100
+        Nx = 200
+        println("Nx is set to ", Nx)
+    end
 
     # NOTE: This is different from the one used in Varga
     x = zeros(Float64, Nx)
@@ -54,6 +65,14 @@ end
         x[i] = a + (i-1)*Δx # calculate x grid point here
     end
 
+    t_last = 0.5
+    Nt = 2000
+    Δt = t_last/(Nt - 1)
+    
+    println("Δt = ", Δt)
+
+    println("k0*t_last = ", k0*t_last)
+
     H = build_Ham(Nx, Δx)
 
     U = inv(I + im*Δt/2 * H)*(I - im*Δt/2 * H)
@@ -61,15 +80,20 @@ end
     #U_RHS = I - im*Δt/2 * H
     #U_LHS = I + im*Δt/2 * H
 
-    σ = sqrt(1.0)
-    k0 = 1.0
-    x0 = 0.0
+    println("2π/k0 = ", 2π/k0)
+    println("Δx    = ", Δx)
+
+    println()
+    println("k0     = ", k0)
+    println("k grid = ", 2π/Δx)
+
+    println("CFL = ", k0*Δt/Δx)
 
     # initial wave function
     t = 0.0
     ψ = zeros(ComplexF64, Nx)
     for i in 1:Nx
-        ψ[i] = gaussian_wavepacket(x[i], t, σ, k0, x0=x0)
+        ψ[i] = gaussian_wavepacket(x[i], t, σ, k0)
     end
 
     # Check norm
@@ -77,12 +101,16 @@ end
 
 
     plot(x, abs.(ψ), label="Abs", linewidth=2, dpi=150)
-    #plot!(x, real.(ψ), label="Re", linewidth=2, dpi=150)
-    #plot!(x, imag.(ψ), label="Im", linewidth=2, dpi=150)
     title!("Initial state")
     ylims!(0.0, 0.8)
-    xlims!(-10, 10)
-    savefig("IMG_itime_" * string(0) * ".png")
+    xlims!(a, b)
+    savefig("IMG_initial.png")
+
+    plot(x, real.(ψ), label="Re", linewidth=2, dpi=150)
+    title!("Initial state")
+    #ylims!(0.0, 0.8)
+    xlims!(0.1*a, 0.1*b)
+    savefig("IMG_initial_Re.png")
 
     t = 0.0
     ψ_ana = zeros(ComplexF64, Nx)
@@ -91,24 +119,23 @@ end
         t += Δt
         @views ψ_new[:] = U * ψ[:]
         for i in 1:Nx
-            ψ_ana[i] = gaussian_wavepacket(x[i], t, σ, k0, x0=x0)
+            ψ_ana[i] = gaussian_wavepacket(x[i], t, σ, k0)
         end
         ψ[:] = ψ_new[:]
     end
+    println("Final t = ", t)
 
     plot(x, abs.(ψ_new), label="Numerical", linewidth=2, dpi=150)
-    #plot!(x, real.(ψ), label="Re", linewidth=2, dpi=150)
-    #plot!(x, imag.(ψ), label="Im", linewidth=2, dpi=150)
     title!("Final state")
     ylims!(0.0, 0.8)
-    xlims!(-10, 10)
-    #plot!(x, abs.(ψ_ana), label="Analytical", linewidth=2, dpi=150)
-    savefig("IMG_itime_" * string(Nt) * ".png")
+    xlims!(a, b)
+    plot!(x, abs.(ψ_ana), label="Analytical", linewidth=2, dpi=150)
+    savefig("IMG_final.png")
 
     @printf("After numerical propagation: norm of the wavepacket: %18.10f\n", dot(ψ,ψ)*Δx)
 
-#end
-#main()
+end
+main()
 
 
 
