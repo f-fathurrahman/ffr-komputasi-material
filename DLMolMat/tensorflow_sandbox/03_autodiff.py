7dc9699601ce0456db96c716b44a042d46f0d03b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-11-24 06:10:54 +0700

    More autodiff

--- DLMolMat/tensorflow_sandbox/03_autodiff.py
+++ DLMolMat/tensorflow_sandbox/03_autodiff.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 # # Diferensiasi Otomatis (Automatic Diferentiation)
 
 # %% [markdown]
+# ## Intro
+
+# %% [markdown]
 # Untuk menghitungan turunan fungsi secara otomatis, TensorFlow perlu merekam proses perhitungan apasaja yang dilakukan.
 
 # %% [markdown]
@@ -95,7 +98,7 @@ for var, g in zip(layer.trainable_variables, grad):
 # A trainable variable
 x0 = tf.Variable(0.5, name='x0')
 # Not trainable
-x1 = tf.Variable(3.0, name='x1', trainable=False)
+x1 = tf.Variable(3.0, name='x1', trainable=True)
 # Not a Variable: A variable + tensor returns a tensor.
 x2 = tf.Variable(2.0, name='x2') + 1.0
 # Not a variable
@@ -108,5 +111,46 @@ grad = tape.gradient(y, [x0, x1, x2, x3])
 
 for g in grad:
   print(g)
+
+# %% [markdown]
+# You can list the variables being watched by the tape using the GradientTape.watched_variables method:
+
+# %%
+[var.name for var in tape.watched_variables()]
+
+# %% [markdown]
+# To record gradients with respect to a tf.Tensor, you need to call GradientTape.watch(x):
+
+# %%
+x = tf.constant(3.0)
+with tf.GradientTape() as tape:
+  tape.watch(x)
+  y = x**2
+
+# dy = 2x * dx
+dy_dx = tape.gradient(y, x)
+print(dy_dx.numpy())
+
+# %%
+[var.name for var in tape.watched_variables()]
+
+# %% [markdown]
+# Conversely, to disable the default behavior of watching all tf.Variables, set watch_accessed_variables=False when creating the gradient tape. This calculation uses two variables, but only connects the gradient for one of the variables:
+
+# %%
+x0 = tf.Variable(2.0)
+x1 = tf.Variable(10.0)
+
+with tf.GradientTape(watch_accessed_variables=False) as tape:
+    tape.watch(x1)
+    y0 = tf.math.sin(x0)
+    y1 = tf.nn.softplus(x1)
+    y = y0 + y1
+    ys = tf.reduce_sum(y)
+
+# dys/dx1 = exp(x1) / (1 + exp(x1)) = sigmoid(x1)
+grad = tape.gradient(ys, {'x0': x0, 'x1': x1})
+
+grad
 
 # %%
