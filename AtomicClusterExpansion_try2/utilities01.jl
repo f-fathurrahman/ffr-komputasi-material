39e97d7920f8166a5e1997585111364d34324b6e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-03-23 00:44:49 +0700

    Recursive show_fields, first version

--- AtomicClusterExpansion_try2/utilities01.jl
+++ AtomicClusterExpansion_try2/utilities01.jl
@@ -5,4 +5,59 @@ function show_fields(a)
         f = getfield(a, s)
         println("\n  fieldname = $s, type = $(typeof(f))")
     end
+    return
+end
+
+function _is_type_skipped(t; indent=" ")
+    if t <: Tuple
+        println(indent * "Stopped here")
+        return true
+    end
+    if t <: StaticArrays.SMatrix
+        println(indent * "Stopped here")
+        return true
+    end
+    if t <: StaticArrays.SVector
+        println(indent * "Stopped here")
+        return true
+    end
+    if t <: Vector
+        println(indent * "Stopped here")
+        return true
+    end
+    return false
+end
+
+function show_fields_recursive(a; indent=" ")
+    t = typeof(a)
+    println()
+    println(indent * "------------------------")
+    println(indent * "Parent type: $t")
+    println(indent * "------------------------")
+    
+    if _is_type_skipped(t, indent=indent)
+        return
+    end
+
+    for s in fieldnames(t)
+        f = getfield(a, s)
+        println("\n" * indent * " - fieldname = $s, type = $(typeof(f))")
+    end
+    #
+    for s in fieldnames(t)
+        f = getfield(a, s)
+        t = typeof(f)
+        if _is_type_skipped(t, indent=indent)
+            continue
+        end
+        if isbitstype(t)
+            continue
+        end
+        if t <: Vector
+            @info "a is a Vector, processing its element instead"
+            a = first(a)
+        end
+        show_fields_recursive(f, indent=indent*" ")
+    end
+    return
 end
