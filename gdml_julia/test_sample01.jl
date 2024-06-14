4ac8f13179a9bd00460ac5e2577782423603f07a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-06-14 19:25:03 +0700

    Draft stratified sampling

--- gdml_julia/test_sample01.jl
+++ gdml_julia/test_sample01.jl
@@ -1,6 +1,6 @@
 using Serialization: deserialize
 using Statistics: quantile
-using StatsBase: sample
+using StatsBase: sample, ProbabilityWeights
 
 E_all = deserialize("E.dat")
 
@@ -41,7 +41,7 @@ reduced_cnts = min.(reduced_cnts, cnts_all)
 reduced_cnts_delta = Nsamples - sum(reduced_cnts)
 
 # ??? only satisfied for reduced_cnts_delta == 0
-#while abs(reduced_cnts_delta) > 0
+while abs(reduced_cnts_delta) > 0
 
     idxsg1 = reduced_cnts .> 1
     max_bin_reduction = minimum(reduced_cnts[idxsg1]) - 1
@@ -55,4 +55,21 @@ reduced_cnts_delta = Nsamples - sum(reduced_cnts)
     uniq_outstanding = unique(outstanding)
     cnts_outstanding = [count(==(u), outstanding) for u in uniq_outstanding]
 
-#end
+    # Bucket IDs to Idxs.
+    outstanding_bucket_idx = findall(in.(uniq_outstanding, uniq_all))
+    println("outstanding_bucket_idx = ", outstanding_bucket_idx)
+
+    reduced_cnts[outstanding_bucket_idx] += (sign(reduced_cnts_delta) .* cnts_outstanding)
+    print("Updated reduced_cnts")
+
+    reduced_cnts_delta = Nsamples - sum(reduced_cnts)
+
+end
+
+idxs_train = Vector{Int64}()
+for (uniq_idx, bin_cnt) in zip(uniq_all, reduced_cnts)
+    println("uniq_idx = ", uniq_idx, " bin_cnt = ", bin_cnt)
+    idx_in_bin_all = findall(idxs .== uniq_idx)
+    append!(idxs_train, sample(idx_in_bin_all, bin_cnt, replace=false))
+end
+
