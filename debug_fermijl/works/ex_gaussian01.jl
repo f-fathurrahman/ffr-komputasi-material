03a412a82f9552f6c86b8beb18e37c0b309d918e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-06-28 18:07:00 +0700

    Investigate basis set

--- debug_fermijl/works/ex_gaussian01.jl
+++ debug_fermijl/works/ex_gaussian01.jl
@@ -1,7 +1,23 @@
 using MyGaussianBasis
 
 # %% Construct a basis set with a given molecule
-b1 = BasisSet("sto-3g", """
+bs_HCl = BasisSet("sto-3g", """
 H         0.00      0.00     0.00
 Cl        0.76      0.00     0.00""")
 
+# The actual basis functions for each atom
+bs_HCl.basis[1] # for H of each of type Vector{BasisFunction}
+bs_HCl.basis[2] # for Cl
+
+Nbasis_H = length(bs_HCl.basis[1])
+Nbasis_Cl = length(bs_HCl.basis[2])
+
+for (i,b) in enumerate(bs_HCl.basis[1])
+    println("\nBasis number: $i")
+    display(b)
+end
+
+for (i,b) in enumerate(bs_HCl.basis[2])
+    println("\nBasis number: $i")
+    display(b)
+end
