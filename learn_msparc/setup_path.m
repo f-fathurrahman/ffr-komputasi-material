9ecdf789bf88e0c22f0092ffb96326f106f004f3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-07-08 11:52:11 +0700

    Try to bypass msparc function

--- learn_msparc/setup_path.m
+++ learn_msparc/setup_path.m
@@ -1,9 +1,22 @@
+clear all; close all;
+
 if ispc
-    addpath(join([pwd, '\src']))  % windows
+    delim_char = '\';  % windows
 else % isunix
-    addpath(join([pwd, '/src']))
+    delim_char = '/';
 end
+% It seems that addpath will also recognize / as path separator in Windows
+
+addpath(join([pwd, delim_char, 'src']));
+
+% For other functions
+% These will be executed by msparc function
+addpath(join([pwd, delim_char, 'src', delim_char, 'xc']));
+addpath(join([pwd, delim_char, 'src', delim_char, 'xc', delim_char, 'exx']));
+addpath(join([pwd, delim_char, 'src', delim_char, 'xc', delim_char, 'mgga']));
+addpath(join([pwd, delim_char, 'src', delim_char, 'xc', delim_char, 'vdW']));
 
 % Then call msparc(prefix) where prefix is input file prefix (string)
 % Example:
 % msparc('SiH4_quick')
+
---
+++ learn_msparc/test_SiH4_quick.m
@@ -0,0 +1,5 @@
+setup_path;
+
+S = initialization('TEMP_SiH4_quick');
+
+
