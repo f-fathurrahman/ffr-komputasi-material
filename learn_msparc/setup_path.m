a8deb18145e3d57022adea79f90c390b235b5999
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-07-08 11:33:08 +0700

    Forget to join strings, add checks for OS

--- learn_msparc/setup_path.m
+++ learn_msparc/setup_path.m
@@ -1,2 +1,9 @@
-addpath([pwd, '\src'])  % windows
+if ispc
+    addpath(join([pwd, '\src']))  % windows
+else % isunix
+    addpath(join([pwd, '/src']))
+end
+
 % Then call msparc(prefix) where prefix is input file prefix (string)
+% Example:
+% msparc('SiH4_quick')
