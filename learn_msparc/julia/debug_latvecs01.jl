aebc46f1bfc16fd3114ba93a2a36a6d6dc6607c6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-07-14 21:15:00 +0700

    Fix lapc_T for LatVecs stored by columns

--- learn_msparc/julia/debug_latvecs01.jl
+++ learn_msparc/julia/debug_latvecs01.jl
@@ -1,8 +1,11 @@
 using LinearAlgebra: dot, norm, det
 include("gen_lattice.jl")
 
-BCx = :PERIODIC_BC
+# Here we prepare LatVecs
+
+# We also need to decide whether we store the vectors in rows or columns
 STORED_BY = :COLUMNS
+@assert STORED_BY in [:ROWS, :COLUMNS]
 
 a = 2.0
 b = 3.0
@@ -10,16 +13,13 @@ c = 5.0
 α = 90.0
 β = 90.0
 γ = 60.0
-
-# Using PWDFT.jl convention LatVecs are stored by columns
-#LatVecs = gen_lattice_fcc(6.0)
 LatVecs = gen_lattice_triclinic( a, b, c, α, β, γ )
+# Using PWDFT.jl convention LatVecs are stored by columns
 
-if STORED_BY == :COLUMNS
-    LatVecs = LatVecs'
-    # transpose because the code below assumes stored by rows
+# If
+if STORED_BY == :ROWS
+    LatVecs = Matrix(LatVecs')
 end
-
 
 # Transformation matrix
 Tinv = zeros(Float64, 3, 3)
@@ -31,45 +31,73 @@ T = inv(Tinv)
 
 # Check the cell typ (orthogonal or non-orthogonal)
 SMALL = 1e-12
-cond12 = abs( dot(LatVecs[1,:], LatVecs[2,:]) ) > SMALL
-cond23 = abs( dot(LatVecs[2,:], LatVecs[3,:]) ) > SMALL
-cond31 = abs( dot(LatVecs[3,:], LatVecs[1,:]) ) > SMALL
+if STORED_BY == :ROWS
+    cond12 = abs( dot(LatVecs[1,:], LatVecs[2,:]) ) > SMALL
+    cond23 = abs( dot(LatVecs[2,:], LatVecs[3,:]) ) > SMALL
+    cond31 = abs( dot(LatVecs[3,:], LatVecs[1,:]) ) > SMALL
+else
+    # :COLUMNS
+    cond12 = abs( dot(LatVecs[:,1], LatVecs[:,2]) ) > SMALL
+    cond23 = abs( dot(LatVecs[:,2], LatVecs[:,3]) ) > SMALL
+    cond31 = abs( dot(LatVecs[:,3], LatVecs[:,1]) ) > SMALL
+end
+
+
 CELL_TYPE = :ORTHOGONAL
 if any([cond12, cond23, cond31]) # or using || operator
    CELL_TYPE = :NON_ORTHOGONAL
 end
+
 println("CELL_TYPE = $(CELL_TYPE)")
+println("STORED_BY = $(STORED_BY)")
 
 
 
 # Normalize
 UnitLatVecs = zeros(Float64, 3, 3)
-UnitLatVecs[1,:] = LatVecs[1,:]/norm(LatVecs[1,:])
-UnitLatVecs[2,:] = LatVecs[2,:]/norm(LatVecs[2,:])
-UnitLatVecs[3,:] = LatVecs[3,:]/norm(LatVecs[3,:])
+if STORED_BY == :ROWS
+    UnitLatVecs[1,:] = LatVecs[1,:]/norm(LatVecs[1,:])
+    UnitLatVecs[2,:] = LatVecs[2,:]/norm(LatVecs[2,:])
+    UnitLatVecs[3,:] = LatVecs[3,:]/norm(LatVecs[3,:])
+else
+    # :COLUMNS
+    UnitLatVecs[:,1] = LatVecs[:,1]/norm(LatVecs[:,1])
+    UnitLatVecs[:,2] = LatVecs[:,2]/norm(LatVecs[:,2])
+    UnitLatVecs[:,3] = LatVecs[:,3]/norm(LatVecs[:,3])
+end
+
 
 # Jacobian
-JacobianUnitLatVecs = det(UnitLatVecs')
+if STORED_BY == :ROWS
+    JacobianUnitLatVecs = det(UnitLatVecs')
+else
+    # :COLUMNS
+    JacobianUnitLatVecs = det(UnitLatVecs)
+end
 if JacobianUnitLatVecs < 0
     error("Volume is negative!")
 end
+
 # metric_T, Gradient and Laplacian transformation matrices
-metric_T = UnitLatVecs * UnitLatVecs'
-#metric_T[1,2] = 2*metric_T[1,2]  # factor of 2 removed?
-#metric_T[2,3] = 2*metric_T[2,3] 
-#metric_T[1,3] = 2*metric_T[1,3]
-grad_T = inv(UnitLatVecs')
+if STORED_BY == :ROWS
+    metric_T = UnitLatVecs * UnitLatVecs'
+    grad_T = inv(UnitLatVecs')
+else
+    # :COLUMNS
+    metric_T = UnitLatVecs' * UnitLatVecs
+    grad_T = inv(UnitLatVecs)
+end
+
 lapc_T = grad_T * grad_T'
-#lapc_T[1,2] = 2*lapc_T[1,2]
-#lapc_T[2,3] = 2*lapc_T[2,3]
-#lapc_T[1,3] = 2*lapc_T[1,3]
+
 
 # Will metric_T, lapc_T depends on whether LatVecs are stored by columns or row?
 
 
-println("lapc_T")
-display(lapc_T); println()
-# These are used for Laplacian matrix
-println("lapc_T[1,2] = ", lapc_T[1,2])
-println("lapc_T[2,3] = ", lapc_T[2,3])
-println("lapc_T[1,3] = ", lapc_T[1,3])
+println("These two should be the same:")
+
+println("lapc_T = ")
+display(lapc_T)
+
+println("T = ")
+display(T)
