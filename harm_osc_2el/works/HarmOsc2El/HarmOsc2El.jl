638d5ee041c81f1e742eb85b9344f8bfe439660a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-21 17:06:31 +0700

    Export more symbols

--- harm_osc_2el/works/HarmOsc2El/HarmOsc2El.jl
+++ harm_osc_2el/works/HarmOsc2El/HarmOsc2El.jl
@@ -5,12 +5,14 @@ using TensorOperations: @tensor
 
 # This will include various other files
 include("system.jl")
+
 # Orbital systems
 export System, Basis, reference_energy, particle_density
 export SpatialSystem, SpatialBasis, HOBasis, SpinBasis
 export PairingSystem, pairing_exact, pairing_MBPT2, Pairing
 export CalogeroSutherland, HOCoulomb, HarmonicOscillator
-
+export HF, energy
+export compute_ground_state!
 
 include("mixer.jl")
 include("util_methods.jl")
