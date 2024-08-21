0085117cc3fe74773ab3caaacf97ec17b444174a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-21 17:06:11 +0700

    Fix HF

---
+++ harm_osc_2el/works/main01.jl
@@ -0,0 +1,32 @@
+using HarmOsc2El
+
+function setup_rhf_sys(;n, l)
+    ω = 0.25
+    basis = SpinBasis(HOBasis(l, ω))
+
+    V = HOCoulomb(ω, shielding = 0.25)
+    grid = [x for x in range(-10, stop = 10, length = 2001)]
+    system = System(n, basis, grid, V);
+
+    rhf = RHF(system)
+    t = @elapsed compute_ground_state!(rhf);
+    rhf_system = System(rhf)
+    println("Reference energy: $(reference_energy(rhf_system)), $(t) s")
+
+    return rhf_system
+end
+
+function hf_energy(; n, l)
+    ω = 0.25
+    basis = SpinBasis(HOBasis(l, ω))
+
+    V = HOCoulomb(ω, shielding = 0.25)
+    grid = [x for x in range(-10, stop = 10, length = 2001)]
+    system = System(n, basis, grid, V);
+
+    hf = HF(system)
+    t = @elapsed compute_ground_state!(hf);
+    println("HF energy: $(energy(hf)), $(t) s")
+end
+
+hf_energy(; n=2, l=20)
