343626c008d44305f293f0323100b93617e0a85c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-24 17:06:56 +0700

    Do not return state

--- harm_osc_2el/works/MyModule/calc_ground_state.jl
+++ harm_osc_2el/works/MyModule/calc_ground_state.jl
@@ -1,27 +1,29 @@
-function calc_ground_state!(state; max_iter = 1000, tol = 1e-10, verbose = 0)
+function calc_ground_state!(state; max_iter=1000, tol=1e-10, verbose = 0)
+    
+    @info "--------------------------------"
+    @info "Calculating ground state"
+    @info "typeof(state) = $(typeof(state))"
+    @info "--------------------------------"
+    
     old_E = energy(state)
     delta_E = 0
     for i in 1:max_iter
         update!(state)
-        
+        #
         new_E = energy(state)
         delta_E = old_E - new_E
-
+        #
         if abs(delta_E) < tol
             if verbose != 0
-                println("Iteration $i: E = $new_E")
+                @printf("Converged !!!")
             end
-
-            return state
+            return
         end
-        if verbose == 1 || (verbose == 2 && (i <= 3 || i >= max_iter - 3))
-            println("Iteration $i: E = $new_E")
-        end
-        if verbose == 3
-            print("\rIteration $i: E = $new_E                           ")
+        if verbose == 1
+            @printf("Iteration %5d   E = %18.10f    ΔE = %15.5e\n", i, new_E, delta_E)
         end
         old_E = new_E
     end
     println("Did not converge after $(max_iter) iterations. Final energy change was $(delta_E)")
-    return state
+    return
 end
