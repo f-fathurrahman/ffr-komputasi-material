2272ac4bf9c33e0bfd0da9305f1c47644b8141ee
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-02-13 10:25:00 +0700

    Remove loops

--- manifold_opt/test_noneig.m
+++ manifold_opt/test_noneig.m
@@ -19,7 +19,9 @@ function test_noneig
 % nlist = [2000 3000 5000 8000 10000];
 % plist = [10 20 30 50];
 % alist = [1 10 100 1000];
-nlist = 2000; plist = 10; alist = 100;
+n = 2000;
+p = 10;
+alpha = 100;
 
 % set tolerance for ARNT
 gtol = 1e-12;
@@ -30,68 +32,60 @@ if ~exist(filesrc, 'dir');     mkdir(filesrc);   end
 filepath = strcat(filesrc, filesep, 'noneig');
 if ~exist(filepath, 'dir');    mkdir(filepath);  end
 
-% loop
-for n = nlist
-    for p = plist
-        for alpha = alist
-            
-            seed = 2010;
-            if exist('RandStream','file')
-                RandStream.setGlobalStream(RandStream('mt19937ar','seed',seed));
-            else
-                disp('WARNING: Not setting random seed')
-                % set seed for what?
-                %randrot('state', seed);
-                %randn('state',seed);
-            end
-            
-            fprintf('\n------- (n,p,alpha) = (%d, %d, %.1f)----\n',n,p, alpha);
-            
-            % generate L
-            L = gallery('tridiag', n, -1, 2, -1);
-            [Ll,Lu] = lu(L);
-            
-            % intial point
-            X = randn(n, p);
-            [U, ~, V] = svd(X, 0);
-            X_init = U*V';
-            tempM2 = alpha*(L\(sum(X_init.^2,2)));
-            tempM2 = spdiags(tempM2,0,n,n);
-            tempM = L + tempM2;
-            [U0, ~, ~] = eigs(tempM, p,'sm');
-            X0 = U0;
-            
-            % Stiefel manifold
-            M = stiefelfactory(n,p);
-            
-            % identify Eulcidean gradient and Hessian
-            opts.hess = @hess;
-            opts.grad = @grad;
-            opts.fun_extra = @fun_extra;
-            
-            % set default parameters for ARNT
-            opts.record = 2; % 0 for slient, 1 for outer iter. info., 2 or more for all iter. info.
-            opts.xtol = 1e-12;
-            opts.ftol = 1e-12;
-            opts.gtol = gtol;
-            opts.maxit = 500;
-            opts.tau = 1;
-            opts.usenumstab = 1;
-            
-            % run ARNT
-            recordname = strcat(filepath,filesep,'Date_',...
-                num2str(date),'noneig','n',num2str(n),'p',num2str(p),'alpha',num2str(alpha),'.txt');
-            opts.recordFile = recordname;
-            t0 = tic; [~,~, out_ARNT] = arnt(X0, @fun, M, opts); tsolve_ARNT = toc(t0);
-                        
-            % print info. in command line
-            fprintf('ARNT|  f: %8.6e, nrmG: %2.1e, cpu: %4.2f, OutIter: %3d, InnerIter: %4d, nfe: %4d,\n',...
-                out_ARNT.fval, out_ARNT.nrmG, tsolve_ARNT, out_ARNT.iter, sum(out_ARNT.iter_sub), out_ARNT.nfe);
-            
-        end
-    end
-    
+% loop      
+seed = 2010;
+if exist('RandStream','file')
+    RandStream.setGlobalStream(RandStream('mt19937ar','seed',seed));
+else
+    disp('WARNING: Not setting random seed')
+    % set seed for what?
+    %randrot('state', seed);
+    %randn('state',seed);
 end
+
+fprintf('\n------- (n,p,alpha) = (%d, %d, %.1f)----\n',n,p, alpha);
+
+% generate L
+L = gallery('tridiag', n, -1, 2, -1);
+[Ll,Lu] = lu(L);
+
+% intial point
+X = randn(n, p);
+[U, ~, V] = svd(X, 0);
+X_init = U*V';
+tempM2 = alpha*(L\(sum(X_init.^2,2)));
+tempM2 = spdiags(tempM2,0,n,n);
+tempM = L + tempM2;
+[U0, ~, ~] = eigs(tempM, p,'sm');
+X0 = U0;
+
+% Stiefel manifold
+M = stiefelfactory(n,p);
+
+% identify Eulcidean gradient and Hessian
+opts.hess = @hess;
+opts.grad = @grad;
+opts.fun_extra = @fun_extra;
+
+% set default parameters for ARNT
+opts.record = 2; % 0 for slient, 1 for outer iter. info., 2 or more for all iter. info.
+opts.xtol = 1e-12;
+opts.ftol = 1e-12;
+opts.gtol = gtol;
+opts.maxit = 500;
+opts.tau = 1;
+opts.usenumstab = 1;
+
+% run ARNT
+recordname = strcat(filepath,filesep,'Date_',...
+    num2str(date),'noneig','n',num2str(n),'p',num2str(p),'alpha',num2str(alpha),'.txt');
+opts.recordFile = recordname;
+t0 = tic; [~,~, out_ARNT] = arnt(X0, @fun, M, opts); tsolve_ARNT = toc(t0);
+            
+% print info. in command line
+fprintf('ARNT|  f: %8.6e, nrmG: %2.1e, cpu: %4.2f, OutIter: %3d, InnerIter: %4d, nfe: %4d,\n',...
+    out_ARNT.fval, out_ARNT.nrmG, tsolve_ARNT, out_ARNT.iter, sum(out_ARNT.iter_sub), out_ARNT.nfe);
+
 
 %
 % Inner functions
