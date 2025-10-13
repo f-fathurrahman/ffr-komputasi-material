92f604048eb3a1d9ebdb9d0ec5796177fba61f1d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-10-13 11:09:57 +0700

    Start debug

--- learn_jrystal/test_01.py
+++ learn_jrystal/test_01.py
@@ -1,9 +1,15 @@
 from prepare_args_01 import args
-print(args)
-
 import my_jrystal as jr
-config = jr.config.get_config(args.config)
 
-# Calculate energy
-jr.calc.energy_normcons(config)
-#jr.calc.energy_all_electrons(config)
+
+def main_debug():
+    print(args)
+    config = jr.config.get_config(args.config)
+    # Calculate energy
+    jr.calc.energy_normcons(config)
+    #jr.calc.energy_all_electrons(config)
+
+
+# XXX: We must write it like this due to some multiprocessing stuffs
+if __name__ == "__main__":
+    main_debug()
